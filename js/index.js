acfe8ffacd7efef5fa046bed11fbf3b466f32b23
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2023-03-13 15:50:20 -0600

    Add 'click' event-listener to New Game button

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// Add 'click' event-listener to the 'New Game' button.
+const newGameButton = document.getElementById("new-game");
+newGameButton.addEventListener("click", () =>
+    console.log(
+        "New Game button: Click Event Triggered"
+    )
+);
