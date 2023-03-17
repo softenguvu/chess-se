c625ae0c00a7cdac3d9ab038d30d93de66ebbbe6
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2023-03-17 13:50:40 -0600

    Import the required chess piece classes

---
+++ js/board.js
@@ -0,0 +1,7 @@
+import { Bishop } from "./bishop";
+import { King } from "./king";
+import { Knight } from "./knight";
+import { Pawn } from "./pawn";
+import { Piece } from "./piece";
+import { Queen } from "./queen";
+import { Rook } from "./rook";
