159feffbf09247335011d78c42c7d855fbe5adb8
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2023-03-20 14:34:41 -0600

    Create constructor for Board

--- js/board.js
+++ js/board.js
@@ -10,5 +10,19 @@ import { Rook } from "./rook";
  * Represents an 8x8 chess board that contains chess pieces.
  */
 export class Board {
+    /**
+     * Retrieves the singleton instance of Board.
+     * 
+     * @returns The singleton instance of Board.
+     */
+    constructor() {
+        // Create singleton instance if necessary.
+        if (Board._instance) {
+            return Board._instance;
+        }
+        Board._instance = this;
 
+        // 2D array of Piece.
+        this.board = Array.from(new Array(8), () => new Array(8));
+    }
 }
