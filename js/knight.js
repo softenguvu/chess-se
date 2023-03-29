ec9c41fce33852a3b858ad903d2687fbd1a50d8f
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2023-03-29 16:45:58 -0600

    Implement constructor() in Knight class

--- js/knight.js
+++ js/knight.js
@@ -4,5 +4,21 @@ import { Piece } from "./piece.js";
  * Represents a knight chess piece that is used with the Board class.
  */
 export class Knight extends Piece {
-
+    /**
+     * Instantiates an instance of Knight with the provided position and piece
+     * id. The new instance belongs to the player with the provided player id.
+     * 
+     * @param {int} id Piece's id.
+     * @param {int} rowPos Piece's row position.
+     * @param {int} colPos Piece's column position.
+     * @param {int} playerId Player id that the piece belongs to.
+     */
+    constructor(id, rowPos, colPos, playerId) {
+        super();
+        this.id = id;
+        this.rowPos = rowPos;
+        this.colPos = colPos;
+        this.playerId = playerId;
+        this.unicodeChar = "&#9816;";  // White knight chess piece.
+    }
 }
