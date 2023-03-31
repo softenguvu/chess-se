ed5f5e0444ee6d465a3e9c712d7aae76eccfdaed
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2023-03-30 21:26:34 -0600

    Implement possibleMoves() in Knight class

--- js/knight.js
+++ js/knight.js
@@ -21,4 +21,84 @@ export class Knight extends Piece {
         this.playerId = playerId;
         this.unicodeChar = "&#9816;";  // White knight chess piece.
     }
+
+    /**
+     * Determines all possible moves of knight at current position.
+     * 
+     * @param {[[Piece]]} board Chess board (2D array of chess pieces).
+     * @returns List of row-column indices on the chess board that indicate all
+     * possible moves of knight at current position.
+     */
+    possibleMoves(board) {
+        return [
+            ...this._movesLeftRight("left", board),
+            ...this._movesLeftRight("right", board)
+        ];
+    }
+
+    /**
+     * Determines the possible moves to left or right of knight.
+     *
+     * @param {string} side Which side of knight to check for possible moves.
+     * Valid args: "left" | "right"
+     * @param {[[Piece]]} board Chess board (2D array of chess pieces).
+     * @returns List of row-column indices on the chess board that indicate
+     * possible moves to left or right of knight.
+     */
+    _movesLeftRight(side, board) {
+        const validSides = ["left", "right"];
+        if (!validSides.includes(side)) {
+            throw new Error(`'${side}' is not a valid side`);
+        }
+
+        let possibleMoves = [];
+        // Check moves below then above knight.
+        const negateRowDiff = [false, false, true, true];
+        // Check moves left or right of knight.
+        const negateColDiff = (side === "left") ? true : false;
+        for (
+            // Offset from knight's current position.
+            let i = 0, rowDiff = 2, colDiff = 1;
+            i < 4;
+            ++i, [rowDiff, colDiff] = [colDiff, rowDiff]
+        ) {
+            const row = this.rowPos + Math.pow(-1, negateRowDiff[i]) * rowDiff;
+            const col = this.colPos + Math.pow(-1, negateColDiff) * colDiff;
+            if (this._possibleMove(row, col, board)) {
+                possibleMoves.push([row, col]);
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    /**
+     * Determines if the provided position is a valid possible move.
+     * 
+     * @param {int} row Row position.
+     * @param {int} col Column position.
+     * @param {[[Piece]]} board Chess board (2D array of chess pieces).
+     * @returns Whether the provided position is a valid possible move.
+     */
+    _possibleMove(row, col, board) {
+        return this._withinBounds(row, col, board) && (
+            (board[row][col]) ?  // Check if chess piece is present at position.
+                board[row][col].getPlayerId() !== this.playerId :
+                true
+        );
+    }
+
+    /**
+     * Determines if the provided position is within boundaries of the provided
+     * chess board.
+     * 
+     * @param {int} row Row position.
+     * @param {int} col Column position.
+     * @param {[[Piece]]} board Chess board (2D array of chess pieces).
+     * @returns Whether the provided position is within boundaries of the
+     * provided chess board.
+     */
+    _withinBounds(row, col, board) {
+        return row >= 0 && row < board.length && col >= 0 && col < board.length;
+    }
 }
