c627e9828119175f3f818cfeeac64f7559b57c30
slayerhintze <bradenhintze@gmail.com>
2023-03-31 19:07:31 -0600

    Added undo to undo button event listener

--- js/index.js
+++ js/index.js
@@ -9,5 +9,5 @@ newGameButton.addEventListener("click", () =>
 // Add 'click' event-listener to the 'Undo' button.
 const undoButton = document.getElementById("undo-move");
 undoButton.addEventListener("click", () =>
-    console.log("Undo button: Click Event Triggered")
+    this.board.lastPieceMoved.undo()
 );
