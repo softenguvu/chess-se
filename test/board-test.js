b16aaf1903364fffbc18a161e66dae011a0825f2
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2023-04-14 21:15:33 -0600

    Add beforeEach() hook for Board tests

--- test/board-test.js
+++ test/board-test.js
@@ -4,5 +4,9 @@ import { Board } from  "../js/board.js";
  * Test suite for the Board class.
  */
 QUnit.module("Board", hooks => {
+    let board;
 
+    hooks.beforeEach(() => {
+        board = new Board();
+    });
 });
