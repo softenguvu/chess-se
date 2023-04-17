764305822871d0543c85f0b11e4d1906a3dca60b
slayerhintze <bradenhintze@gmail.com>
2023-04-17 16:49:55 -0600

    More fixes

--- js/pawn.js
+++ js/pawn.js
@@ -36,11 +36,11 @@ export class Pawn extends Piece{
                 possibleMoves.push([this.rowPos+moveDir, this.colPos]);
             }
 
-            if (0 <= this.colPos+moveDir && this.colPos+moveDir <= 7 && board[this.rowPos+moveDir][this.colPos+moveDir].getPlayerId() != playerId) { // Check the pieces attackable locations for pieces
+            if (0 <= this.colPos+moveDir && this.colPos+moveDir <= 7 && board[this.rowPos+moveDir][this.colPos+moveDir] != null && board[this.rowPos+moveDir][this.colPos+moveDir].getPlayerId() != this.playerId) { // Check the pieces attackable locations for pieces
                 possibleMoves.push([this.rowPos+moveDir, this.colPos+moveDir]);
             }
 
-            if (0 <= this.colPos-moveDir && this.colPos-moveDir <= 7 && board[this.rowPos+moveDir][this.colPos-moveDir].getPlayerId() != playerId) { // Check the pieces attackable locations for pieces
+            if (0 <= this.colPos-moveDir && this.colPos-moveDir <= 7 && board[this.rowPos+moveDir][this.colPos+moveDir] != null && board[this.rowPos+moveDir][this.colPos-moveDir].getPlayerId() != this.playerId) { // Check the pieces attackable locations for pieces
                 possibleMoves.push([this.rowPos+moveDir, this.colPos-moveDir]);
             }
         }
