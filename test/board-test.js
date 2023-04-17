d4572e4fe6a0c6fdbb3aaf45c66c9b37a0cc2905
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2023-04-17 17:11:55 -0600

    Add test for Board's initBoard()

--- test/board-test.js
+++ test/board-test.js
@@ -1,4 +1,6 @@
 import { Board } from  "../js/board.js";
+import { Rook } from "../js/rook.js";
+import { Pawn } from "../js/pawn.js";
 
 /**
  * Test suite for the Board class.
@@ -65,6 +67,66 @@ QUnit.module("Board", hooks => {
             board.lastPieceMoved,
             null,
             "Verify [lastPieceMoved] member is now null."
+        );
+    });
+
+    /**
+     * Test initBoard().
+     */
+    QUnit.test("initBoard()", assert => {
+        board.initBoard();
+        assert.deepEqual(
+            board.board[0][0],
+            new Rook(16, 0, 0, 1),
+            "Verify [board] member contains black Rook at A8."
+        );
+
+        assert.deepEqual(
+            board.board[7][7],
+            new Rook(7, 7, 7, 0),
+            "Verify [board] member contains white Rook at H1."
+        );
+
+        assert.deepEqual(
+            board.board[1][1],
+            new Pawn(25, 1, 1, 1),
+            "Verify [board] member contains black Pawn at B7."
+        );
+
+        assert.deepEqual(
+            board.board[6][6],
+            new Pawn(14, 6, 6, 0),
+            "Verify [board] member contains white Pawn at G2."
+        );
+
+        const playerOneId = 0;
+        assert.true(
+            board.takenPieces.has(playerOneId),
+            "Verify [takenPieces] member has key for player one id."
+        );
+
+        assert.deepEqual(
+            board.takenPieces.get(playerOneId),
+            [],
+            "Verify [takenPieces] member has empty array for player one."
+        );
+
+        const playerTwoId = 1;
+        assert.true(
+            board.takenPieces.has(playerTwoId),
+            "Verify [takenPieces] member has key for player two id."
+        );
+
+        assert.deepEqual(
+            board.takenPieces.get(playerTwoId),
+            [],
+            "Verify [takenPieces] member has empty array for player one."
+        );
+
+        assert.deepEqual(
+            board.lastPieceMoved,
+            null,
+            "Verify [lastPieceMoved] member is still null."
         );
     });
 });
