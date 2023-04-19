3d7c06694b5630103dbe4a1c6ee8ae16592519ab
Jacob Bañuelos <72123087+jbeBan@users.noreply.github.com>
2023-04-19 14:50:20 -0600

    Add tests for Queen's constructor()

--- test/queen-test.js
+++ test/queen-test.js
@@ -1,1 +1,66 @@
-import { Queen } from "../js/queen";
+import { Queen } from "../js/queen.js";
+
+/**
+ * Test suite for the Queen class.
+ */
+QUnit.module("Queen", hooks => {
+    /**
+     * Test constructor().
+     */
+    QUnit.test("constructor()", assert => {
+        const pieceId = 0;
+        const rowPos = 0;
+        const colPos = 0;
+        const playerId = 0;
+
+        const queen = new Queen(pieceId, rowPos, colPos, playerId);
+        assert.deepEqual(
+            queen.getId(),
+            pieceId,
+            "Verify [id] member is initialized correctly."
+        );
+
+        assert.deepEqual(
+            queen.getRowPos(),
+            rowPos,
+            "Verify [rowPos] member is initialized correctly."
+        );
+
+        assert.deepEqual(
+            queen.getColPos(),
+            colPos,
+            "Verify [colPos] member is initialized correctly."
+        );
+
+        assert.deepEqual(
+            queen.prevRowPos,
+            null,
+            "Verify [prevRowPos] member is null."
+        );
+
+        assert.deepEqual(
+            queen.prevColPos,
+            null,
+            "Verify [prevColPos] member is null."
+        );
+
+        assert.deepEqual(
+            queen.lastTake,
+            null,
+            "Verify [lastTake] member is null."
+        );
+
+        assert.deepEqual(
+            queen.getPlayerId(),
+            playerId,
+            "Verify [playerId] member is initialized correctly."
+        );
+
+        const blackQueenUnicode = "&#9819;";
+        assert.deepEqual(
+            queen.unicodeChar,
+            blackQueenUnicode,
+            "Verify [unicodeChar] member is initialized as black queen."
+        );
+    });
+});
