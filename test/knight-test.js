f7e08a33bef25a4380206a0074c3293480e50448
Kyle Tanner <15ktanner@gmail.com>
2023-04-23 15:54:45 -0600

    Add test cases for Knight class

--- test/knight-test.js
+++ test/knight-test.js
@@ -37,19 +37,14 @@ QUnit.module("Knight", hooks => {
             "Verify {column} is 0."
         );
         assert.deepEqual(
-            knight.unicodeChar,
-            "U+265E",
-            "Verify {unicodeChar} is correct."
-        );
-        assert.deepEqual(
             knight.playerId,
             0,
             "Verify {playerId} is 0."
         );
         assert.deepEqual(
-            knight.lastTake,
-            null,
-            "Verify {lastTake} is null."
+            knight.unicodeChar,
+            "U+265E",
+            "Verify {unicodeChar} is correct."
         );
     });
 
@@ -105,17 +100,62 @@ QUnit.module("Knight", hooks => {
     });
     
     /**
-     * Test possibleMoves() with other pieces
+     * Test possibleMoves() with friendly pieces
      */
-    QUnit.test("possibleMoves() with other pieces", assert => {
+    QUnit.test("possibleMoves() with friendly pieces", assert => {
         let knight = new Knight(0, 3, 3, 0);
         let knight1 = new Knight(1, 2, 1, 0);
-        let knight2 = new Knight(2, 1, 2, 1);
+        let knight2 = new Knight(2, 1, 2, 0);
+        let knight3 = new Knight(3, 1, 4, 0);
+        let knight4 = new Knight(4, 2, 5, 0);
+        let knight5 = new Knight(5, 4, 1, 0);
+        let knight6 = new Knight(6, 5, 2, 0);
+        let knight7 = new Knight(7, 5, 4, 0);
+        let knight8 = new Knight(8, 4, 5, 0);
         board.board[2][1] = knight1;
         board.board[1][2] = knight2;
+        board.board[1][4] = knight3;
+        board.board[2][5] = knight4;
+        board.board[4][1] = knight5;
+        board.board[5][2] = knight6;
+        board.board[5][4] = knight7;
+        board.board[4][5] = knight8;
+        let possibleMoves = knight.possibleMoves(board.board);
+        let expectedMoves = [];
+        possibleMoves.sort();
+        expectedMoves.sort();
+        assert.deepEqual(
+            JSON.stringify(possibleMoves),
+            JSON.stringify(expectedMoves),
+            "Verify {possibleMoves()} with friendly pieces"
+        );
+
+    });
+
+    /**
+     * Test possibleMoves() with enemy pieces
+     */
+    QUnit.test("possibleMoves() with enemy pieces", assert => {
+        let knight = new Knight(0, 3, 3, 0);
+        let knight1 = new Knight(1, 2, 1, 1);
+        let knight2 = new Knight(2, 1, 2, 1);
+        let knight3 = new Knight(3, 1, 4, 1);
+        let knight4 = new Knight(4, 2, 5, 1);
+        let knight5 = new Knight(5, 4, 1, 1);
+        let knight6 = new Knight(6, 5, 2, 1);
+        let knight7 = new Knight(7, 5, 4, 1);
+        let knight8 = new Knight(8, 4, 5, 1);
+        board.board[2][1] = knight1;
+        board.board[1][2] = knight2;
+        board.board[1][4] = knight3;
+        board.board[2][5] = knight4;
+        board.board[4][1] = knight5;
+        board.board[5][2] = knight6;
+        board.board[5][4] = knight7;
+        board.board[4][5] = knight8;
         let possibleMoves = knight.possibleMoves(board.board);
         let expectedMoves = [
-            [1,2], [1, 4], [2,5],
+            [1,2], [1, 4], [2,1], [2,5],
             [4,1], [4, 5], [5,2], [5,4]
         ];
         possibleMoves.sort();
@@ -123,9 +163,7 @@ QUnit.module("Knight", hooks => {
         assert.deepEqual(
             JSON.stringify(possibleMoves),
             JSON.stringify(expectedMoves),
-            "Verify {possibleMoves()} with other pieces on the board"
+            "Verify {possibleMoves()} with enemy pieces"
         );
-
     });
-
 });
